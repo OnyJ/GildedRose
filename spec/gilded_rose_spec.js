0d559612d045f794c1d0951e18c2c5cf32f33ac5
OnyJ <julien.josselin75@hotmail.fr>
2020-05-20 17:32:45 +0200

    finished basic products Design Pattern and specs testing

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,4 +1,4 @@
-var { Shop, Item } = require('../src/gilded_rose.js');
+var { Shop, Item, BasicProduct } = require("../src/gilded_rose.js");
 describe("GildedRose shop manager", function () {
   var listItems;
 
@@ -6,38 +6,63 @@ describe("GildedRose shop manager", function () {
     listItems = [];
   });
 
-
-  it("Baisser de 1 la qualité et sellIn d'item normaux", function () {
+  it("Reduces by 1 Basic Items's quality and sellIn", function () {
     listItems.push(new Item("+5 Dexterity Vest", 10, 20));
     listItems.push(new Item("Mana Cake", 3, 6));
 
-    const gildedRose = new Shop(listItems);
-    const items = gildedRose.updateQuality();
+    // Original test
+    // const gildedRose = new Shop(listItems);
+    // const items = gildedRose.updateQuality();
+    //
+    // var expected = [
+    // { sellIn: 9, quality: 19 },
+    // { sellIn: 2, quality: 5 },
+    // ];
+    // expected.forEach(function (testCase, idx) {
+    // // // //console.log(items[idx]);
+    // expect(items[idx].quality).toBe(testCase.quality);
+    // expect(items[idx].sellIn).toBe(testCase.sellIn);
+    // });
 
-    var expected = [
+    // My Test
+    var expectedV2 = [
       { sellIn: 9, quality: 19 },
-      { sellIn: 2, quality: 5 }
+      { sellIn: 2, quality: 5 },
     ];
-    expected.forEach(function (testCase, idx) {
-      expect(items[idx].quality).toBe(testCase.quality);
-      expect(items[idx].sellIn).toBe(testCase.sellIn);
+
+    let myTestItems = [];
+    listItems.forEach((item) => {
+      const basicProduct = new BasicProduct(item);
+      myTestItems.push(basicProduct.update());
+    });
+
+    expectedV2.forEach(function (testCase, idx) {
+      expect(myTestItems[idx].quality).toBe(testCase.quality);
+      expect(myTestItems[idx].sellIn).toBe(testCase.sellIn);
     });
   });
 
-  it("Augmenter la qualité de 1 pour Aged Brie et Backstage passes", function () {
-    listItems.push(new Item("Aged Brie", 20, 30));
-    listItems.push(new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30));
+  // it("Augmenter la qualité de 1 pour Aged Brie et Backstage passes", function () {
+  // listItems.push(new Item("Aged Brie", 20, 30));
+  // listItems.push(
+  // new Item("Backstage passes to a TAFKAL80ETC concert", 20, 30)
+  // );
+  //
+  // const gildedRose = new Shop(listItems);
+  // const items = gildedRose.updateQuality();
 
-    const gildedRose = new Shop(listItems);
-    const items = gildedRose.updateQuality();
+  // My test
+  // const agedBrie = new AgedBrie(listItems)
+  // const items =
 
-    var expected = [
-      { sellIn: 19, quality: 31 },
-      { sellIn: 19, quality: 31 },
-    ];
-    expected.forEach(function (testCase, idx) {
-      expect(items[idx].quality).toBe(testCase.quality);
-      expect(items[idx].sellIn).toBe(testCase.sellIn);
-    });
-  });
+  //
+  // var expected = [
+  // { sellIn: 19, quality: 31 },
+  // { sellIn: 19, quality: 31 },
+  // ];
+  // expected.forEach(function (testCase, idx) {
+  // expect(items[idx].quality).toBe(testCase.quality);
+  // expect(items[idx].sellIn).toBe(testCase.sellIn);
+  // });
+  // });
 });
--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -4,6 +4,105 @@ class Item {
     this.sellIn = sellIn;
     this.quality = quality;
   }
+
+  updateQuality() {}
+}
+
+// class Shop2 {
+// constructor(items = []) {
+// this.items = items;
+// }
+//
+// updateQuality() {
+// this.items.forEach((item) => {});
+// }
+// }
+
+class AgedBrie {
+  constructor(item) {
+    this.item = item;
+  }
+  // - si qualité < 50 alors qualité++
+  // - si sellin < 11 changeQualité = 2
+  // - si sellin < 6 changeQualité = 3
+  // - si sellin === 0 qualité = 0
+  // - si périmé, qualité diminue * 2
+
+  increaseQuality(qualityChange) {
+    if (this.item.quality < 50) this.item.quality += qualityChange;
+  }
+
+  defineQualityChange() {
+    let qualityChange = 1;
+    if (this.item.sellIn < 11) qualityChange = 2;
+    if (this.item.sellIn < 6) qualityChange = 3;
+    return qualityChange;
+  }
+
+  checkExpired() {
+    if (this.item.sellIn === 0) this.quality = 0;
+  }
+
+  update() {
+    const qualityChange = this.defineQualityChange();
+    this.increaseQuality(qualityChange);
+    this.checkExpired();
+  }
+}
+
+class Backstage {
+  constructor() {}
+
+  update() {
+    // - si qualité < 50 alors qualité++
+    // - si sellin < 11 changeQualité = 2
+    // - si sellin < 6 changeQualité = 3
+    // - si sellin === 0 qualité = 0
+    // - si périmé, qualité diminue * 2
+  }
+}
+
+class Sulfuras {
+  // - qualité = 80
+}
+
+class IsConjured {
+  constructor() {}
+
+  check() {
+    // - si .name.includes("Conjured")
+    // updateConjured =>
+    // - si qualité > 0 qualité -= 2 * changeQualité
+    // - si périmé, qualité diminue * 4
+  }
+}
+
+class BasicProduct {
+  constructor(item) {
+    this.item = item;
+  }
+  // - si sellin > 0 alors --
+  // - si qualité > 0 alors --
+  // - si périmé, qualité diminue * 2
+
+  updateSellIn(sellInChange) {
+    if (this.item.sellIn > 0) this.item.sellIn -= sellInChange;
+  }
+
+  updateQuality(qualityChange) {
+    if (this.item.quality > 0) this.item.quality -= qualityChange;
+  }
+
+  updateExpired() {
+    if (this.item.sellIn === 0) this.updateQuality(2);
+  }
+
+  update() {
+    this.updateSellIn(1);
+    this.updateQuality(1);
+    this.updateExpired();
+    return this.item;
+  }
 }
 
 class Shop {
@@ -13,32 +112,6 @@ class Shop {
   updateQuality() {
     // global :
     // - toutes qualités comprises entre 0 et 50
-
-    // updateAgedBrie =>
-    // - si qualité < 50 alors qualité++
-    // - si sellin < 11 changeQualité = 2
-    // - si sellin < 6 changeQualité = 3
-    // - si sellin === 0 qualité = 0
-    // - si périmé, qualité diminue * 2
-
-    // updateBackStage =>
-    // - si qualité < 50 alors qualité++
-    // - si sellin < 11 changeQualité = 2
-    // - si sellin < 6 changeQualité = 3
-    // - si sellin === 0 qualité = 0
-    // - si périmé, qualité diminue * 2
-
-    // updateSulfuras =>
-    // - si qualité < 50 alors qualité++
-
-    // checkConjured =>
-    // - si .name.includes("Conjured")
-    // updateConjured =>
-    // - si qualité > 0 qualité -= 2 * changeQualité
-
-    // updateBasicProduct =>
-    // - si sellin > 0 alors --
-    // - si qualité > 0 alors --
 
     for (var i = 0; i < this.items.length; i++) {
       if (
@@ -100,4 +173,5 @@ class Shop {
 module.exports = {
   Item,
   Shop,
+  BasicProduct,
 };
