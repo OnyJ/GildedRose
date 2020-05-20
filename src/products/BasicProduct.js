2f440faaec013691ec31e9e3260c38dd5af7fc57
OnyJ <julien.josselin75@hotmail.fr>
2020-05-20 19:48:04 +0200

    clean basicproduct file

--- src/products/BasicProduct.js
+++ src/products/BasicProduct.js
@@ -1,33 +1,4 @@
-// import Item from "../Item";
-
-// class FirstBasicProduct extends Item {
-// constructor(item) {
-// this.item = item;
-// }
-// updateSellIn(sellInChange) {
-// if (this.item.sellIn > 0) this.item.sellIn -= sellInChange;
-// }
-//
-// updateQuality(qualityChange) {
-// if (this.item.quality > 0) this.item.quality -= qualityChange;
-// }
-//
-// updateExpired() {
-// if (this.item.sellIn === 0) this.updateQuality(2);
-// }
-//
-// update() {
-// this.updateSellIn(1);
-// this.updateQuality(1);
-// this.updateExpired();
-// return this.item;
-// }
-// }
-
-// var { Item } = require("../Item");
-
 export default class BasicProduct {
-  // class BasicProduct {
   constructor(item) {
     this.item = item;
   }
